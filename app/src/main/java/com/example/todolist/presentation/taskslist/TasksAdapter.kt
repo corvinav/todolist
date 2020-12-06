1833ad146a20cfbf8a812303e5575b27dc30e576
corvinav <corvinav@gmail.com>
2020-12-06 17:31:46 +0200

    add some ui

---
+++ app/src/main/java/com/example/todolist/presentation/taskslist/TasksAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.todolist.presentation.taskslist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.TaskItemBinding
+import com.example.todolist.domain.entities.Task
+
+class TasksAdapter():
+    ListAdapter<Task, TasksAdapter.ViewHolder>(TaskDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    class ViewHolder private constructor(private val binding: TaskItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Task){
+            binding.task = item
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    class TaskDiffCallback : DiffUtil.ItemCallback<Task>() {
+        override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/com/example/todolist/presentation/taskslist/TasksListFragment.kt
+++ app/src/main/java/com/example/todolist/presentation/taskslist/TasksListFragment.kt
@@ -1,12 +1,13 @@
 package com.example.todolist.presentation.taskslist
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.todolist.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.databinding.TasksListFragmentBinding
 
 class TasksListFragment : Fragment() {
 
@@ -14,17 +15,36 @@ class TasksListFragment : Fragment() {
         fun newInstance() = TasksListFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    lateinit private var binding: TasksListFragmentBinding
+    lateinit private var tasksAdapter: TasksAdapter
+    private val viewModel: MainViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = TasksListFragmentBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        initTasksList()
+
+        if (savedInstanceState == null) {
+            viewModel.loadList()
+        }
     }
 
+
+    private fun initTasksList() {
+        val tasksRV = binding.tasksRV
+        tasksAdapter = TasksAdapter()
+        tasksRV.layoutManager = LinearLayoutManager(this.context)
+        tasksRV.adapter = tasksAdapter
+
+        viewModel.tasks.observe(viewLifecycleOwner) {
+            tasksAdapter.submitList(it)
+        }
+    }
 }
